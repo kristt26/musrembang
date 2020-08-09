9963882e125f1e04e1f2a604ee7cad61c99403a6
kristt26 <kristt26@gmail.com>
2020-08-10 03:16:41 +0900

    ok

--- application/models/admin/Profile_model.php
+++ application/models/admin/Profile_model.php
@@ -1,6 +1,6 @@
 <?php
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Profile_model extends CI_Model {
     function select()
